415fc6e2711cdce05e009d5b0fd44050c776e770
allarendil <tebicat516@gmail.com>
2023-10-10 15:26:39 -0400

    Basic grid and page setup

--- main.js
+++ main.js
@@ -3,20 +3,23 @@ const container = document.querySelector('.container');
 const div = document.createElement('div');
 div.style.color = 'blue';
 div.style.csstext = 'color: blue; background: white;';
+div.style.border = 'pink dotted 10px';
 div.textContent = "hey There!";
 container.appendChild(div);
 
 const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = "Some Text";
+content.style.border = 'black solid 5px';
 container.appendChild(content);
 
 const para = document.createElement('p');
 para.textContent = "hey I'm red!";
-para.style.color = 'red';
+para.style.border = 'red solid';
 container.appendChild(para);
 
 const box = document.createElement('div');
-box.style.border = 'solid, black, 10px';
+box.style.border = 'solid black 5px';
+box.textContent = 'blank'
 box.style.backgroundColor = 'pink';
 container.appendChild(box);
