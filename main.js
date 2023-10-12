aa30356cef8660cf3f38a6137d28eb1550b851f7
allarendil <tebicat516@gmail.com>
2023-10-12 13:42:55 -0400

    Create Grid Layout

--- main.js
+++ main.js
@@ -1,25 +1,12 @@
 const container = document.querySelector('.container');
 
-const div = document.createElement('div');
-div.style.color = 'blue';
-div.style.csstext = 'color: blue; background: white;';
-div.style.border = 'pink dotted 10px';
-div.textContent = "hey There!";
-container.appendChild(div);
+function createGrid(num) {
+    for (let rows = 0; rows < num; rows++) {
+        for (let columns = 0; columns < num; columns++) {
+            let grid = document.createElement('div');
+            container.appendChild(grid).className = 'grid';
+        };
+    };
+};
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = "Some Text";
-content.style.border = 'black solid 5px';
-container.appendChild(content);
-
-const para = document.createElement('p');
-para.textContent = "hey I'm red!";
-para.style.border = 'red solid';
-container.appendChild(para);
-
-const box = document.createElement('div');
-box.style.border = 'solid black 5px';
-box.textContent = 'blank'
-box.style.backgroundColor = 'pink';
-container.appendChild(box);
+createGrid(16);
